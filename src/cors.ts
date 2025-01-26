ae54b8d998fea4ea56f880b41764a2ed7b602ec2
cch137 <96408836+cch137@users.noreply.github.com>
2025-01-27 02:10:14 +0800

    release 0.1.17

--- src/cors.ts
+++ src/cors.ts
@@ -1,18 +1,28 @@
 import type { JetRequest, JetResponse } from "./http.js";
 
-export default function cors(
-  req: JetRequest,
-  res: JetResponse,
-  next: () => void
-) {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.setHeader("Content-Length", "0");
-    res.status(204).end();
-  } else {
-    next();
-  }
+type CORSOptions = {
+  credentials?: string;
+  origin?: string;
+  methods?: string;
+  headers?: string;
+};
+
+export default function cors({
+  credentials = "true",
+  origin = "*",
+  methods = "*",
+  headers = "*",
+}: CORSOptions = {}) {
+  return (req: JetRequest, res: JetResponse, next: () => void) => {
+    res.setHeader("Access-Control-Allow-Credentials", credentials);
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", methods);
+    res.setHeader("Access-Control-Allow-Headers", headers);
+    if (req.method === "OPTIONS") {
+      res.setHeader("Content-Length", "0");
+      res.status(204).end();
+    } else {
+      next();
+    }
+  };
 }
